5382e4f4ce5466fc369504733aad79dd2f1abeca
SantiagoBonillaGuevara <sbonilla284@gmail.com>
2025-05-19 09:56:24 -0500

    Creacion de Rol tecaher, profile, forgot password, etc...

--- app/src/main/java/com/example/gimnasioalemn_friedrichvonschiller/AnnotationsAndFollowUp.kt
+++ app/src/main/java/com/example/gimnasioalemn_friedrichvonschiller/AnnotationsAndFollowUp.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class StartStudents : AppCompatActivity() {
+class AnnotationsAndFollowUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_start_students)
+        setContentView(R.layout.activity_annotations_and_follow_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
